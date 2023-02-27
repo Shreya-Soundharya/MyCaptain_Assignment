26ce629bf38964c1303ef9ad4305390c81278a72
Shreya Soundharya <113111043+Shreya-Soundharya@users.noreply.github.com>
2023-02-27 19:50:07 +0530

    Add files via upload

---
+++ Positive numbers.py
@@ -0,0 +1,14 @@
+l1=[12, -7, 5, 64, -14] 
+print("Input: list1 = ",l1)
+l2=[]
+for i in l1:
+    if i>0:
+        l2.append(i)
+print("Output: ",l2)
+l3=[12, 14, -95, 3]
+print("Input: list2 = ",l3)
+l4=[]
+for i in l3:
+    if i>0:
+        l4.append(i)
+print("Output: ",l4)
